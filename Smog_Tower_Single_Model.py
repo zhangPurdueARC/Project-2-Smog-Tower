3e9cbb0f670ca00811408f6e03fe1e6fad69b914
zhangPurdueARC <zhan4833@purdue.edu>
2024-03-19 17:24:02 -0400

    first update

--- Smog_Tower_Single_Model.py
+++ Smog_Tower_Single_Model.py
@@ -1,1 +1,31 @@
+import math
+
+# smog calculations
+
+def diameterToRadius(diameter):
+    return diameter / 2
+
+def sphereVolume(radius):
+    return 4 / 3 * math.pi * (radius ** 3)
+
+# vector fields
+
+def gravityAccel():
+    return 9.8
+
+def hydroStaticPressureAccel():
+    pass # return to work on
+
+# approximate smog particles as sphere
+# 10 micrometers diameter variant
+
+diameter10um = 10 * (10 ** -6)
+radius10um = diameterToRadius(diameter10um)
+volume10um = sphereVolume(radius10um)
+
+# 2.5 micrometers diameter variant
+
+diameter5HalvesUm = 2.5 * (10 ** -6)
+radius5HalvesUm = diameterToRadius(diameter5HalvesUm)
+volume5HalvesUm = sphereVolume(radius10um)
 
