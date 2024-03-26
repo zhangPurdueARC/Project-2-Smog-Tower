306efcab22e902a8a9f48d3bf9e2bb66adb5d0f6
zhangPurdueARC <zhan4833@purdue.edu>
2024-03-25 21:38:02 -0400

    Update

--- Smog_Tower_Single_Model.py
+++ Smog_Tower_Single_Model.py
@@ -1,5 +1,8 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['agg.path.chunksize'] = 1000000
 # smog calculations
 # approximate smog particles as sphere
 
@@ -48,22 +51,18 @@ class Tower: # approximate the tower as a line charge
         for i in range(0, 2, 1):
             if (self.captureParticles[i][0] == particle.diameter):
                 self.captureParticles[i][1] += particle.numParticles
-                self.kgRemoved[i] += particle.numParticles * particle.mass
+                self.kgRemoved[i] += particle.mass
                 break
 
 # vector fields
 def gravityAccel():
     return np.array([0, 0, -9.81])
 
-def hydroStaticPressureAccel(particle): # needs mass
-    atmoPressure = 1.013 * (10 ** 5) - 1.28 * 9.81 * particle.position[2] 
-    return np.array([0, 0, -1 * atmoPressure * particle.crossSectionalArea / particle.mass])
-
 def buoyantAccel(particle):
     return np.array([0, 0, 1.28 * 9.81 * particle.volume / particle.mass])
 
-def dragAccel(particle, windVelocity):
-    return -6 * math.pi * (1.895 * (10 ** -5)) * particle.diameter * np.array(particle.velocity + windVelocity) / particle.mass
+def dragAccel(particle):
+    return -6 * math.pi * (1.895 * (10 ** -5)) * particle.diameter * np.array(particle.velocity) / particle.mass
 
 def electricFieldAccel(particle, summedElectricField):
     return summedElectricField * particle.charge / particle.mass
@@ -71,17 +70,11 @@ def electricFieldAccel(particle, summedElectricField):
 def cubeSort(cubes):
     return sorted(cubes, key = lambda particle : particle.position[2])
 
-"""
-def windAccel(particle, windVelocity): # worry about this later
-    return
-"""
-
 cubes = []
-windVelocity = np.array([0, 0, 0])
 
-for x in range(1, 301, 1): # 3 meters radius, 7 meters tall area
-    for z in range(1, 180, 1):
-        position = [x / 100, 0, z /100]
+for x in range(1, 701, 1): # 7 meters radius, 4 meters tall area
+    for z in range(1, 400, 1):
+        position = [x / 100, 0, z / 100]
         # 10 micrometers diameter variant
         cubes.append(Particle(10 * (10 ** -6), 4.89 * 10 ** -8, position))
         # 2.5 micrometers diameter variant
@@ -93,50 +86,54 @@ for cube in cubes:
 
 tower = Tower(1.9 * 10 ** -7, 7)
 
-maxTime = 20 * 60
+maxTime = 0.1
 timeStep = 0.01 
 currentTime = 0
-while (currentTime <= maxTime):
-    cubes = cubeSort(cubes)
-    capturedCubes = []
-    below = 0
-    above = 0
-    for cube in cubes: # ignoring charge overlaps, calculates density as though it were alone on a layer, ignoring pushes!
-        charge = cube.charge * cube.numParticles
-        above += charge / 0.01 / 3 / 2 / (8.854 * 10 ** -12)
-    print(currentTime)
-    for cube in cubes:
-        charge = cube.charge * cube.numParticles
-        below += charge / 0.01 / 3 / 2 / (8.854 * 10 ** -12)
-        above -= charge / 0.01 / 3 / 2 / (8.854 * 10 ** -12)
-        electricField = tower.electricField(cube) + [0, 0, above] + [0, 0, -1 * below]
-        accel = electricFieldAccel(cube, electricField) + dragAccel(cube, windVelocity) + buoyantAccel(cube) + hydroStaticPressureAccel(cube) + gravityAccel()
-        velocity = cube.velocity
-        position = cube.position + 0.5 * accel * timeStep ** 2 + velocity * timeStep
-        if (position[0] < 0):
-            tower.capture(cube)
-            capturedCubes.append(cube)
-        elif (position[2] < 0):
-            position[2] = 0
-        elif (position[2] > 7):
-            position[2] = 7
-        cube.velocity = velocity + accel * timeStep
-        cube.position = position
-    print(currentTime)
-    for cube in capturedCubes:
-        cubes.remove(cube)
-    currentTime += timeStep
+with open("results.txt", "w") as file:
+    while (currentTime <= maxTime):
+        cubes = cubeSort(cubes)
+        capturedCubes = []
+        x = []
+        z = []
+        below = 0
+        above = 0
+        for cube in cubes: # ignoring charge overlaps, calculates density as though it were alone on a layer, ignoring pushes!
+            charge = cube.charge * cube.numParticles
+            above += charge / 0.01 / 3 / 2 / (8.854 * 10 ** -12)
+        file.write(str(currentTime)+": ")
+        print(currentTime)
+        for cube in cubes:
+            charge = cube.charge * cube.numParticles
+            below += charge / 0.01 / 3 / 2 / (8.854 * 10 ** -12)
+            above -= charge / 0.01 / 3 / 2 / (8.854 * 10 ** -12)
+            electricField = tower.electricField(cube) + [0, 0, above] + [0, 0, -1 * below]
+            accel = electricFieldAccel(cube, electricField) + dragAccel(cube) + buoyantAccel(cube) + gravityAccel()
+            velocity = cube.velocity
+            position = cube.position + 0.5 * accel * timeStep ** 2 + velocity * timeStep
+            if (position[0] < 0):
+                tower.capture(cube)
+                capturedCubes.append(cube)
+            else:
+                if (position[2] < 0):
+                    position[2] = 0
+                elif (position[2] > 7):
+                    position[2] = 7
+                cube.velocity = velocity + accel * timeStep
+                cube.position = position
+                x.append(cube.position[0]) # not quite working, why?, ending up with strage graphs
+                z.append(cube.position[2])
+        plt.plot(x, z)
+        file.write(str(x))
+        file.write("\n")
+        file.write(str(z))
+        file.write("\n")
+        plt.savefig(str(currentTime)+".png")
+        for cube in capturedCubes:
+            cubes.remove(cube)
+        file.write(str(len(cubes))+"\n")
+        currentTime += timeStep
 
 print(tower.captureParticles)
 print(tower.kgRemoved)
 
 print(sum(tower.kgRemoved) / totalMass)
-
-"""
-randZ = -1
-while randZ < 0:
-    randX = random.random()
-    randY = random.random()
-    randZ = 1 - randX - randY
-windVelocity = [math.sqrt(5.81152 ** 2 * randX) * random.choice((-1, 1)), math.sqrt(5.81152 ** 2 * randY) * random.choice((-1, 1)), math.sqrt(5.81152 ** 2 * randZ) * random.choice((-1, 1))]
-"""
--- test.py
+++
@@ -1,2 +0,0 @@
-import numpy as np
-print(np.square([1, 2, 3]))
