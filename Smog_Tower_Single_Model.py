dd50c74c35309e616d76f12f3abcff756f312618
zhangPurdueARC <zhan4833@purdue.edu>
2024-03-26 21:48:29 -0400

    Final Update

--- Smog_Tower_Single_Model.py
+++ Smog_Tower_Single_Model.py
@@ -1,8 +1,5 @@
 import math
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-mpl.rcParams['agg.path.chunksize'] = 1000000
 # smog calculations
 # approximate smog particles as sphere
 
@@ -62,7 +59,14 @@ def buoyantAccel(particle):
     return np.array([0, 0, 1.28 * 9.81 * particle.volume / particle.mass])
 
 def dragAccel(particle):
-    return -6 * math.pi * (1.895 * (10 ** -5)) * particle.diameter * np.array(particle.velocity) / particle.mass
+    speed = math.sqrt(particle.velocity[0] ** 2 + particle.velocity[1] ** 2 + particle.velocity[2] ** 2)
+    if (speed == 0):
+        return [0, 0, 0]
+    RE = 1.28 * particle.diameter * speed / (1.895 * 10 ** -5)
+    dragCoef = 24 / RE
+    force = 0.5 * 1.28 * dragCoef * particle.crossSectionalArea * speed ** 2
+    direction = particle.velocity / speed * -1
+    return force * direction / particle.mass
 
 def electricFieldAccel(particle, summedElectricField):
     return summedElectricField * particle.charge / particle.mass
@@ -72,8 +76,8 @@ def cubeSort(cubes):
 
 cubes = []
 
-for x in range(1, 701, 1): # 7 meters radius, 4 meters tall area
-    for z in range(1, 400, 1):
+for x in range(1, 101, 1): # 4 meters radius, 4 meters tall area
+    for z in range(0, 126, 1):
         position = [x / 100, 0, z / 100]
         # 10 micrometers diameter variant
         cubes.append(Particle(10 * (10 ** -6), 4.89 * 10 ** -8, position))
@@ -81,59 +85,92 @@ for x in range(1, 701, 1): # 7 meters radius, 4 meters tall area
         cubes.append(Particle(2.5 * (10 ** -6), 3.35 * 10 ** -8, position))
 
 totalMass = 0
+total10 = 0
+total5 = 0
 for cube in cubes:
     totalMass += cube.mass
+    if (cube.diameter == 10 * (10 ** -6)):
+        total10 += cube.numParticles
+    else:
+        total5 += cube.numParticles
+
+print(total10)
+print(total5)
 
 tower = Tower(1.9 * 10 ** -7, 7)
 
-maxTime = 0.1
-timeStep = 0.01 
-currentTime = 0
-with open("results.txt", "w") as file:
-    while (currentTime <= maxTime):
-        cubes = cubeSort(cubes)
-        capturedCubes = []
-        x = []
-        z = []
-        below = 0
-        above = 0
-        for cube in cubes: # ignoring charge overlaps, calculates density as though it were alone on a layer, ignoring pushes!
-            charge = cube.charge * cube.numParticles
-            above += charge / 0.01 / 3 / 2 / (8.854 * 10 ** -12)
-        file.write(str(currentTime)+": ")
-        print(currentTime)
-        for cube in cubes:
-            charge = cube.charge * cube.numParticles
-            below += charge / 0.01 / 3 / 2 / (8.854 * 10 ** -12)
-            above -= charge / 0.01 / 3 / 2 / (8.854 * 10 ** -12)
-            electricField = tower.electricField(cube) + [0, 0, above] + [0, 0, -1 * below]
-            accel = electricFieldAccel(cube, electricField) + dragAccel(cube) + buoyantAccel(cube) + gravityAccel()
-            velocity = cube.velocity
-            position = cube.position + 0.5 * accel * timeStep ** 2 + velocity * timeStep
-            if (position[0] < 0):
-                tower.capture(cube)
-                capturedCubes.append(cube)
-            else:
-                if (position[2] < 0):
-                    position[2] = 0
-                elif (position[2] > 7):
-                    position[2] = 7
-                cube.velocity = velocity + accel * timeStep
-                cube.position = position
-                x.append(cube.position[0]) # not quite working, why?, ending up with strage graphs
-                z.append(cube.position[2])
-        plt.plot(x, z)
-        file.write(str(x))
-        file.write("\n")
-        file.write(str(z))
-        file.write("\n")
-        plt.savefig(str(currentTime)+".png")
-        for cube in capturedCubes:
-            cubes.remove(cube)
-        file.write(str(len(cubes))+"\n")
-        currentTime += timeStep
+try:
+    maxTime = 60
+    timeStep = 0.05
+    currentTime = 0
+    with open("results.txt", "w") as file:
+        while (currentTime <= maxTime):
+            cubes = cubeSort(cubes)
+            capturedCubes = []
+            x = []
+            z = []
+            velocityArray = []
+            accelArray = []
+            below = 0
+            above = 0
+            for cube in cubes: # ignoring charge overlaps, calculates density as though it were alone on a layer, ignoring pushes!
+                charge = cube.charge * cube.numParticles
+                above += charge / 0.01 / 3 / 2 / (8.854 * 10 ** -12)
+            file.write(str(currentTime)+": ")
+            print(currentTime)
+            for cube in cubes:
+                charge = cube.charge * cube.numParticles
+                below += charge / 0.01 / 3 / 2 / (8.854 * 10 ** -12)
+                above -= charge / 0.01 / 3 / 2 / (8.854 * 10 ** -12)
+                electricField = tower.electricField(cube) + [0, 0, above] + [0, 0, -1 * below]
+                accel = electricFieldAccel(cube, electricField) + buoyantAccel(cube) + gravityAccel()
+                dragAcceleration = dragAccel(cube)
+                velocity = cube.velocity
+                for i in range(0, 3, 1):
+                    if (abs(accel[i]) < abs(dragAcceleration[i])):
+                        if abs((dragAcceleration[i] + accel[i]) * timeStep) > abs(velocity[i]): # prevent drag from making movement
+                            velocity[i] = 0
+                            accel[i] = 0
+                        else:
+                            accel[i] += dragAcceleration[i]
+                    else:
+                        accel[i] += dragAcceleration[i]
+                position = cube.position + 0.5 * accel * timeStep ** 2 + velocity * timeStep
+                if (position[0] < 0):
+                    tower.capture(cube)
+                    capturedCubes.append(cube)
+                else:
+                    if (position[2] < 0):
+                        position[2] = 0
+                        velocity[2] = 0
+                    elif (position[2] > 7):
+                        position[2] = 7
+                        velocity[2] = 0
+                    cube.velocity = velocity + accel * timeStep
+                    cube.position = position
+                    x.append(cube.position[0]) # not quite working, why?, ending up with strage graphs
+                    z.append(cube.position[2])
+                    velocityArray.append(velocity)
+                    accelArray.append(accel)
+            file.write(str(x))
+            file.write("\n")
+            file.write(str(z))
+            file.write("\n")
+            file.write(str(velocityArray))
+            file.write("\n")
+            file.write(str(accelArray))
+            file.write("\n")
+            for cube in capturedCubes:
+                cubes.remove(cube)
+            file.write(str(len(cubes))+"\n")
+            currentTime += timeStep
 
-print(tower.captureParticles)
-print(tower.kgRemoved)
+    print(tower.captureParticles)
+    print(tower.kgRemoved)
 
-print(sum(tower.kgRemoved) / totalMass)
+    print(sum(tower.kgRemoved) / totalMass)
+except KeyboardInterrupt:
+    print(tower.captureParticles)
+    print(tower.kgRemoved)
+
+    print(sum(tower.kgRemoved) / totalMass)
